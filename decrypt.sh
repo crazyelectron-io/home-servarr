a9dc53f8439b213732dc4074a15b97bb573aa7fc
Ron Moerman <ron@crazyelectron.io>
2025-03-11 11:52:01 +0100

    Remove file exists check from script
    Signed-off-by: Ron Moerman <ron@crazyelectron.io>

--- decrypt.sh
+++ decrypt.sh
@@ -3,12 +3,7 @@
 SOPS_AGE_KEY_FILE=/root/.config/sops/age/keys.txt
 
 # decrypt Bazarr configuration file
-if [! -f ./config/bazarr/config.yaml ]; then
- sops -d ./config/bazarr/config.sops.yaml > ./config/bazarr/config.yaml
-fi
+sops -d ./config/bazarr/config.sops.yaml > ./config/bazarr/config.yaml
 
 # Decrypt Lidarr configuration file
-# XML (binary) file not supported by VSCode extension
-if [! -f ./config/lidarr/config.xml ]; then
-  sops -d --output-type binary ./config/lidarr/config.sops.xml > ./config/lidarr/config.xml
-fi
+sops -d --output-type binary ./config/lidarr/config.sops.xml > ./config/lidarr/config.xml
