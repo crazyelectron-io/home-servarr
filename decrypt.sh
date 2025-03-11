844a1bca553389a083b60618bc898aa0ca21a2b9
Ron Moerman <ron@crazyelectron.io>
2025-03-11 12:04:48 +0100

    Add other collectors
    Signed-off-by: Ron Moerman <ron@crazyelectron.io>

--- decrypt.sh
+++ decrypt.sh
@@ -7,3 +7,9 @@ sops -d ./config/bazarr/config.sops.yaml > ./config/bazarr/config.yaml
 
 # Decrypt Lidarr configuration file
 sops -d --output-type binary ./config/lidarr/config.sops.xml > ./config/lidarr/config.xml
+
+# Decrypt Radarr configuration file
+sops -d --output-type binary ./config/radarr/config.sops.xml > ./config/radarr/config.xml
+
+# Decrypt Redarr configuration file
+sops -d --output-type binary ./config/readarr/config.sops.xml > ./config/readarr/config.xml
--- encrypt.sh
+++ encrypt.sh
@@ -6,8 +6,26 @@ if [ ! -f ./config/bazarr/config.sops.yaml ]; then
   rm ./config/bazarr/config.yaml
 fi
 
-# encrypt Bazarr configuration file (first time)
+# encrypt Lidarr configuration file (first time)
 if [ ! -f ./config/lidarr/config.sops.xml ]; then
   sops -e --input-type binary ./config/lidarr/config.xml > ./config/lidarr/config.sops.xml
   rm ./config/lidarr/config.xml
+fi
+
+# encrypt Radarr configuration file (first time)
+if [ ! -f ./config/radarr/config.sops.xml ]; then
+  sops -e --input-type binary ./config/radarr/config.xml > ./config/radarr/config.sops.xml
+  rm ./config/radarr/config.xml
+fi
+
+# encrypt Readarr configuration file (first time)
+if [ ! -f ./config/readarr/config.sops.xml ]; then
+  sops -e --input-type binary ./config/readarr/config.xml > ./config/readarr/config.sops.xml
+  rm ./config/readarr/config.xml
+fi
+
+# encrypt Sonarr configuration file (first time)
+if [ ! -f ./config/sonarr/config.sops.xml ]; then
+  sops -e --input-type binary ./config/sonarr/config.xml > ./config/sonarr/config.sops.xml
+  rm ./config/sonarr/config.xml
 fi
