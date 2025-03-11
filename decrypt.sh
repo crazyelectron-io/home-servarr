29fcbd9ddf2e0ee773836e2f530e6d2c1fb53623
Ron Moerman <ron@crazyelectron.io>
2025-03-11 11:49:16 +0100

    Update init config for lidarr
    Signed-off-by: Ron Moerman <ron@crazyelectron.io>

--- decrypt.sh
+++ decrypt.sh
@@ -3,7 +3,13 @@
 SOPS_AGE_KEY_FILE=/root/.config/sops/age/keys.txt
 
 # decrypt Bazarr configuration file
-sops -d ./config/bazarr/config.sops.yaml > ./config/bazarr/config.yaml
+# Not needed: done through VS COde extension
+# if [! -f ./config/bazarr/config.yaml ]; then
+#  sops -d ./config/bazarr/config.sops.yaml > ./config/bazarr/config.yaml
+# fi
 
 # Decrypt Lidarr configuration file
-# sops -d --output-type binary ./config/lidarr/config.sops.xml > ./config/lidarr/config.xml
+# XML (binary) file not supported by VSCode extension
+if [! -f ./config/lidarr/config.xml ]; then
+  sops -d --output-type binary ./config/lidarr/config.sops.xml > ./config/lidarr/config.xml
+fi
--- encrypt.sh
+++ encrypt.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# encrypt Bazarr configuration file
-sops -e ./config/bazarr/config.yaml > ./config/bazarr/config.sops.yaml
-rm ./config/bazarr/config.yaml
+# encrypt Bazarr configuration file (first time)
+if [ ! -f ./config/bazarr/config.sops.yaml ]; then
+  sops -e ./config/bazarr/config.yaml > ./config/bazarr/config.sops.yaml
+  rm ./config/bazarr/config.yaml
+fi
 
-# encrypt Bazarr configuration file
-sops -e --input-type binary ./config/lidarr/config.xml > ./config/lidarr/config.sops.xml
-rm ./config/lidarr/config.xml
+# encrypt Bazarr configuration file (first time)
+if [ ! -f ./config/lidarr/config.sops.xml ]; then
+  sops -e --input-type binary ./config/lidarr/config.xml > ./config/lidarr/config.sops.xml
+  rm ./config/lidarr/config.xml
+fi
